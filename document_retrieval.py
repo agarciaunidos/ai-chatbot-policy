12e503fa7b3921f5ad0aa657f143df8ec645cf21
Armando Garcia <agarcia@unidosus.org>
2024-06-07 13:53:22 -0500

    added reranker

--- document_retrieval.py
+++ document_retrieval.py
@@ -8,11 +8,12 @@ import time
 from langchain import hub
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_cohere import CohereRerank
+from langchain.retrievers.contextual_compression import ContextualCompressionRetriever
 
 # Import only necessary components
 from langchain_pinecone import PineconeVectorStore
 from langchain_core.messages import HumanMessage, AIMessage
-from langchain_cohere import CohereRerank
 from aws_secrets_initialization import PINECONE_API_KEY, INDEX_NAME, COHERE_API_KEY, llm,embeddings,dynamodb_history
 
 # Initialize services
@@ -44,8 +45,9 @@ def handle_query_retrieval(query, selected_years, doc_types):
     try:
         conditions = generate_filter_conditions(selected_years, doc_types)
         vector_store = initialize_vector_store(INDEX_NAME)
-        retriever = vector_store.as_retriever(search_kwargs={'filter': conditions, 'k': 20})
-        retrieval_chain = create_retrieval_chain(retriever, documents_chain)
+        base_retriever = vector_store.as_retriever(search_kwargs={'filter': conditions, 'k': 20})
+        compression_retriever = ContextualCompressionRetriever(base_compressor=compressor, base_retriever=base_retriever)
+        retrieval_chain = create_retrieval_chain(compression_retriever, documents_chain)
         filter_details = f"Applying filters: Years {selected_years[0]}-{selected_years[1]}, Types: {', '.join(doc_types) if doc_types else 'All types'}"
         response = retrieval_chain.invoke({"input": query, "filter": filter_details})
         sources_df = format_search_results_as_dataframe(response['context'])
@@ -67,12 +69,14 @@ def format_search_results_as_dataframe(documents):
     for document in documents:
         metadata = document.metadata
         title = metadata.get('title', '')
+        page = metadata.get('page', '')
         source_url = metadata.get('source', '').replace('s3://', 'https://s3.amazonaws.com/')
         doc_type = metadata.get('type', '')
         year = metadata.get('year', '')
+        relevance_score = metadata.get('relevance_score', '')
         if year:
             year = str(int(year))
-        results.append({"Title": title, "Source": source_url, "Type": doc_type, "Year": year})
+        results.append({"Title": title, "Page": page, "Source": source_url, "Type": doc_type, "Year": year, "Relevance Score" : relevance_score})
     return pd.DataFrame(results)
 
 def format_answer_sources(documents):
